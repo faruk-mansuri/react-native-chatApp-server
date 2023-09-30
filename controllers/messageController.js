b64353c059c277bfa0a2270deba759f88bcb76a0
faruq <faruqmansuri707@gmail.com>
2023-09-30 10:17:15 +0530

    add get conversation controller

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,3 +1,4 @@
+import { StatusCodes } from 'http-status-codes';
 import Message from '../models/MessageModel.js';
 
 export const getAllMessage = (req, res) => {
@@ -7,12 +8,28 @@ export const getAllMessage = (req, res) => {
 export const sendMessage = async (req, res) => {
   const { messageType, message } = req.body;
   const { receiverId } = req.params;
-  const data = { senderId: req.user.userId, receiverId, messageType, message };
-  // const newMessage = await Message
 
-  res.send('message send');
+  const newMessage = await Message.create({
+    senderId: req.user.userId,
+    receiverId,
+    messageType,
+    message,
+  });
+
+  res.status(StatusCodes.OK).json({ msg: 'Message sent successfully' });
 };
+export const conversation = async (req, res) => {
+  const { receiverId } = req.params;
 
+  const messages = await Message.findOne({
+    $or: [
+      { senderId: req.user.userId, receiverId: receiverId },
+      { senderId: receiverId, receiverId: req.user.userId },
+    ],
+  }).populate('senderId', '_id name');
+
+  res.status(StatusCodes.OK).json(messages);
+};
 export const receiveMessage = (req, res) => {
   res.send('message receive');
 };
--- routes/MessageRoute.js
+++ routes/MessageRoute.js
@@ -4,9 +4,13 @@ import {
   getAllMessage,
   sendMessage,
   receiveMessage,
+  conversation,
 } from '../controllers/messageController.js';
 import upload from '../middleware/multerMiddleware.js';
 
-router.route('/:receiverId').post(upload.single('avatar'), sendMessage);
+router
+  .route('/:receiverId')
+  .get(conversation)
+  .post(upload.single('avatar'), sendMessage);
 
 export default router;
